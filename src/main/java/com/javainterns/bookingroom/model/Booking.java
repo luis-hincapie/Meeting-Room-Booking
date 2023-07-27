a577904bfe5f68744f808937e9ce7396a65db72e
luis.hincapie <luis.hincapie@blankfactor.com>
2023-07-27 15:07:19 -0500

    Booking entity creation

--- src/main/java/com/javainterns/bookingroom/model/Booking.java
+++ src/main/java/com/javainterns/bookingroom/model/Booking.java
@@ -1,4 +1,77 @@
 package com.javainterns.bookingroom.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne(targetEntity = Room.class)
+    private Room room;
+
+    @ManyToOne(targetEntity = User.class)
+    private User user;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date startTime;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    private Date endTime;
+
+    public Booking() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Room getRoom() {
+        return room;
+    }
+
+    public void setRoom(Room room) {
+        this.room = room;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Booking(Room room, User user, Date startTime, Date endTime) {
+        this.room = room;
+        this.user = user;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 }
