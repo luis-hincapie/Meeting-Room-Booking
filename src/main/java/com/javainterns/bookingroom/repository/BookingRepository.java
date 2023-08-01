039ce005337f95a1ae197148ac87f936ae7d2596
luis.hincapie <luis.hincapie@blankfactor.com>
2023-08-01 10:51:17 -0500

    messages from bundle impl

--- src/main/java/com/javainterns/bookingroom/repository/BookingRepository.java
+++ src/main/java/com/javainterns/bookingroom/repository/BookingRepository.java
@@ -1,14 +1,21 @@
 package com.javainterns.bookingroom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.javainterns.bookingroom.model.Booking;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    // List<Booking> findByDateAndRoom(Date date, Long id);
+    @Query("select b from Booking b where b.room.id = ?1 and b.date = ?2")
+    List<Booking> findByRoom_IdAndDate(Long id, LocalDate date);
 
+
+    // List<Booking> findByDateAndRoom(Date date, Long id);
     // @Query("SELECT END_TIME, START_TIME FROM BOOKING WHERE DATE = ?1 AND ROOM_ID
     // = ?2")
     // Optional<List<Booking>> findBookingsByRoomIdAndDate(Date date, Long id);
--- src/main/java/com/javainterns/bookingroom/service/BookingServiceImpl.java
+++ src/main/java/com/javainterns/bookingroom/service/BookingServiceImpl.java
@@ -7,8 +7,12 @@ import com.javainterns.bookingroom.model.Room;
 import com.javainterns.bookingroom.model.dto.BookingRequest;
 import com.javainterns.bookingroom.model.mapper.BookingRequestMapper;
 import com.javainterns.bookingroom.repository.BookingRepository;
+import com.javainterns.bookingroom.utils.Messages;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+
 
 import java.util.List;
 import java.util.Optional;
@@ -25,6 +29,9 @@ public class BookingServiceImpl implements BookingService {
     @Autowired
     RoomService roomService;
 
+    @Autowired
+    private Messages messages;
+
     @Override
     public BookingRequest create(BookingRequest bookingRequest) {
         Booking booking = bookingRequestMapper.toBooking(bookingRequest);
@@ -37,7 +44,7 @@ public class BookingServiceImpl implements BookingService {
 
     @Override
     public Boolean delete(Long id) {
-        if (!bookingRepository.existsById(id)) throw new NoRecordFoundException("Booking Record Not Found");
+        if (!bookingRepository.existsById(id)) throw new NoRecordFoundException(messages.get("booking.record.not.found"));
         bookingRepository.deleteById(id);
         return true;
     }
