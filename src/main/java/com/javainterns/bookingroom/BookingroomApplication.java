61e96a417f8474ce36adbbaaf3b5fa891ed5059a
luis.hincapie <luis.hincapie@blankfactor.com>
2023-08-09 10:03:57 -0500

    update: SpringDocConfig login endpoint

--- src/main/java/com/javainterns/bookingroom/BookingroomApplication.java
+++ src/main/java/com/javainterns/bookingroom/BookingroomApplication.java
@@ -1,7 +1,9 @@
 package com.javainterns.bookingroom;
 
-import java.util.Set;
-
+import com.javainterns.bookingroom.model.ERole;
+import com.javainterns.bookingroom.model.Role;
+import com.javainterns.bookingroom.model.User;
+import com.javainterns.bookingroom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -9,18 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.javainterns.bookingroom.model.ERole;
-import com.javainterns.bookingroom.model.Role;
-import com.javainterns.bookingroom.model.User;
-import com.javainterns.bookingroom.repository.UserRepository;
-
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import java.util.Set;
 
 @SpringBootApplication
 public class BookingroomApplication {
@@ -72,37 +63,5 @@ public class BookingroomApplication {
       userRepository.save(userEntity2);
       userRepository.save(userEntity3);
     };
-  }
-
-  private SecurityScheme createAPIKeyScheme() {
-    return new SecurityScheme()
-      .type(SecurityScheme.Type.HTTP)
-      .bearerFormat("JWT")
-      .scheme("bearer");
-  }
-
-  @Bean
-  public OpenAPI openAPI() {
-    return new OpenAPI()
-      .addSecurityItem(
-        new SecurityRequirement().addList("Bearer Authentication")
-      )
-      .components(
-        new Components()
-          .addSecuritySchemes("Bearer Authentication", createAPIKeyScheme())
-      )
-      .info(
-        new Info()
-          .title("My REST API")
-          .description("Some custom description of API.")
-          .version("1.0")
-          .contact(
-            new Contact()
-              .name("Sallo Szrajbman")
-              .email("www.baeldung.com")
-              .url("salloszraj@gmail.com")
-          )
-          .license(new License().name("License of API").url("API license URL"))
-      );
   }
 }
--- src/main/java/com/javainterns/bookingroom/controller/SecurityController.java
+++ src/main/java/com/javainterns/bookingroom/controller/SecurityController.java
@@ -1,8 +1,7 @@
 package com.javainterns.bookingroom.controller;
 
-import java.security.Principal;
-
 import jakarta.validation.constraints.NotNull;
+import java.security.Principal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
